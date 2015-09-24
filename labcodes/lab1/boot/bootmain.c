207f46dfa95e96c60b074c603cad8abd27ec2480
zhenglu.zl <zhenglu.zl@alipay.com>
2015-09-24 18:10:48 +0800

    bootmain.c

--- labcodes/lab1/boot/bootmain.c
+++ labcodes/lab1/boot/bootmain.c
@@ -7,53 +7,64 @@
  * an ELF kernel image from the first IDE hard disk.
  *
  * DISK LAYOUT
- *  * This program(bootasm.S and bootmain.c) is the bootloader.
- *    It should be stored in the first sector of the disk.
+ *  * This program(bootasm.S and bootmain.c) is the bootloader.  bootloader由bootasm.S和bootmain.c两个文件组成
+ *    It should be stored in the first sector of the disk.  bootloader应该放在disk中第一个扇区
  *
- *  * The 2nd sector onward holds the kernel image.
+ *  * The 2nd sector onward holds the kernel image.  第二个扇区放置内核镜像
  *
- *  * The kernel image must be in ELF format.
+ *  * The kernel image must be in ELF format.  内核镜像必须为ELF格式
  *
- * BOOT UP STEPS
- *  * when the CPU boots it loads the BIOS into memory and executes it
+ *  启动步骤
+ *  * 当cpu启动的时候， 载入BIOS到内存，之后执行它
  *
- *  * the BIOS intializes devices, sets of the interrupt routines, and
- *    reads the first sector of the boot device(e.g., hard-drive)
- *    into memory and jumps to it.
+ *  * BIOS初始化设备，设置中断例行程序，并且读入启动设备（比如硬盘）的第一个扇区到内存，并且跳转到该程序
  *
- *  * Assuming this boot loader is stored in the first sector of the
- *    hard-drive, this code takes over...
+ *  * 假设该boot loader放置在硬盘的第一个扇区，该代码接管一切
  *
- *  * control starts in bootasm.S -- which sets up protected mode,
- *    and a stack so C code then run, then calls bootmain()
+ *  * 控制起始于bootasm.S, 将CPU设为保护模式， 建立一个栈使得C代码接下去运行，接着调用bootmain()
  *
- *  * bootmain() in this file takes over, reads in the kernel and jumps to it.
+ *  * 该文件中的bootmain接管，读入内核，并且跳转到内核。
  * */
 
-#define SECTSIZE        512
+#define SECTSIZE        512   // 每个扇区为512个字节
 #define ELFHDR          ((struct elfhdr *)0x10000)      // scratch space
 
-/* waitdisk - wait for disk ready */
+/*
+ * waitdisk - 等待硬盘准备好
+ * inb从外部设备读入数据，其中inb(0x1F7)为读取磁盘状态
+ * 死循环到硬盘准备好
+ * */
+
 static void
 waitdisk(void) {
     while ((inb(0x1F7) & 0xC0) != 0x40)
         /* do nothing */;
 }
 
-/* readsect - read a single sector at @secno into @dst */
+/* *
+ * readsect - 读入一个单独扇区@secno到@dst
+ *
+ * @secno: 扇区号
+ * @dst: 目标地址，为void指针
+ * */
+
 static void
 readsect(void *dst, uint32_t secno) {
-    // wait for disk to be ready
+    // 等待硬盘准备好
+    // 死循环
     waitdisk();
 
+    // 下面这里为写硬盘
+    // 需要看硬盘指令手册
     outb(0x1F2, 1);                         // count = 1
-    outb(0x1F3, secno & 0xFF);
-    outb(0x1F4, (secno >> 8) & 0xFF);
-    outb(0x1F5, (secno >> 16) & 0xFF);
-    outb(0x1F6, ((secno >> 24) & 0xF) | 0xE0);
-    outb(0x1F7, 0x20);                      // cmd 0x20 - read sectors
+    outb(0x1F3, secno & 0xFF);              // 写入低八位, 显然这里的数据位为八位
+    outb(0x1F4, (secno >> 8) & 0xFF);       // 写入9-16位
+    outb(0x1F5, (secno >> 16) & 0xFF);      // 写入17-24位
+    outb(0x1F6, ((secno >> 24) & 0xF) | 0xE0);  // 写入25-32位，其中，25-28为扇区号码， 29位为0，30-32为1.
+    outb(0x1F7, 0x20);                      // cmd 0x20 - 读取扇区
 
     // wait for disk to be ready
+    // 阻塞等待
     waitdisk();
 
     // read a sector
